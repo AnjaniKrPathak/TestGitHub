e133bad592d8d982b913bacff7ef4739fabd41c1
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2016-03-04 17:31:23 +0530

    Code change by member 2 .

--- Core_Java_Practice/src/com/java/test/Manager1.java
+++ Core_Java_Practice/src/com/java/test/Manager1.java
@@ -2,6 +2,6 @@ package com.java.test;
 
 public class Manager1 {
 public static void main(String[] args) {
-	System.out.println("HI this is test import code project ");
+	System.out.println("Hi I got the code change ");
 }
 }

c7c80716dbe6279059ee9626f11e7c9a74c0549c
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2016-03-04 17:35:59 +0530

    Change by user3

--- Core_Java_Practice/src/com/java/test/Test.java
+++ Core_Java_Practice/src/com/java/test/Test.java
@@ -3,6 +3,7 @@ package com.java.test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hi ");
-	System.out.println("hi");
+	System.out.println("Project change by user3 ");
+	
 }
 }

2d373d13088aef43e3ce0301d2b03d3219b7e2c4
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2016-03-07 14:21:36 +0530

    Anjani

--- Core_Java_Practice/src/com/java/test/Test.java
+++ Core_Java_Practice/src/com/java/test/Test.java
@@ -3,7 +3,7 @@ package com.java.test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hi ");
-	System.out.println("Project change by Anjani");
+	System.out.println("Project change by Diferent user");
 	
 }
 }

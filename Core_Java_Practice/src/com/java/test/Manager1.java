38d91bfe340c90a1c86f930249f72a60f6a518f0
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2016-03-07 11:44:04 +0530

    Change By Anjani

--- Core_Java_Practice/src/com/java/test/Manager1.java
+++ Core_Java_Practice/src/com/java/test/Manager1.java
@@ -2,6 +2,6 @@ package com.java.test;
 
 public class Manager1 {
 public static void main(String[] args) {
-	System.out.println("Hi I got the code change ");
+	System.out.println("Hi please change the name  ");
 }
 }
--- Core_Java_Practice/src/com/java/test/Test.java
+++ Core_Java_Practice/src/com/java/test/Test.java
@@ -3,7 +3,7 @@ package com.java.test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hi ");
-	System.out.println("Project change by user1");
+	System.out.println("Project change by Anjani");
 	
 }
 }
